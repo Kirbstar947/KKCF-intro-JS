c095a64171c6b84cbf03d29f568f6f36b3db010f
JustinLiao <122176632+Kirbstar947@users.noreply.github.com>
2024-07-31 16:33:53 +0000

    Added a lot of the instructions

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 
 //console log text, expression 
+// console.log("Hello World");
+// console.log(3+6);
 
 // Create a variable to hold your name as a string
-
+let myName = "Justin";
 //Concatenate your name variable into a message and log it to the console.
 //Example: “Hello, Zach!” 
-
+console.log("Hello, " + myName);
 //console log your name variable
 
 //Re-assign the value of your name variable below its declaration and console log it again
-
+myName = "Liao";
+console.log(myName);
 //practice math operators
-
+// console.log(8+5);
+// console.log(8-5);
+// console.log(8*5);
+// console.log(8**5); //exponent
+// console.log(8/5);
+// console.log(8%5); //remainder
+let Uno = (11%5);
 // boolean - true or false
-
+let isSunny = false;
+isSunny = true;
+console.log(isSunny);
+console.log(!isSunny); //opposite
 //create null and undefined
+let isAmazing = null; //null
+let isHungry; //undefined
+
 
 //mix data types
+let UnoMas = " One time";
+console.log("Hurrah " + Uno + UnoMas);
 
+let Five = "5";
+Five = Number(Five);
+console.log(Five);
 //string methods
 
 //link up html file and create prompt and save value
